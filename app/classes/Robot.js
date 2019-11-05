4142f4ef9f348f00b8eb1724c2b7eb5807ce4f66
Andrew Antolino <andrew@smithbrothersmedia.com.au>
2019-11-05 23:29:08 +1100

    Refactor into ES5 - use prototypal inheritance

--- app/classes/Robot.js
+++ app/classes/Robot.js
@@ -1,111 +1,105 @@
-class Robot {
-  constructor() {
-    this.positionX;
-    this.positionY;
-    this.orientation;
-    
-    this.dirObj = {
-      0: 'NORTH',
-      1: 'EAST',
-      2: 'SOUTH',
-      3: 'WEST'
-    };
-  }
-
-  // Set initial orientation when placed on the board
-  turn(direction) {
-    // Rotate robot 90 degrees depending on input
-    if (direction == 'LEFT') {
-      // Move to the orientation left of current orientation
-      // e.g. if current orientation == 'N' and input == 'LEFT', set orientation to 'W'
-      // if current orientation == 'E' and input == 'RIGHT', set orientation to 'S'
-      // etc.
-      let newOrientation;
-      
-      Object.keys(this.dirObj).forEach(key => {
-        if (this.dirObj[key] == this.orientation) {
-          // validate key:
-          // if key - 1 == -1, no index exists
-          key = key - 1 == -1 ? 0 : parseInt(key) - 1;
-          newOrientation = this.dirObj[key];
-        }
-      });
-
-      return this.orientation = newOrientation;
-
-    } else if (direction == 'RIGHT') {
-      let newOrientation;
-      
-      Object.keys(this.dirObj).forEach(key => {
-        
-        if (this.dirObj[key] == this.orientation) {
-          // validate key:
-          // if key + 1 == 4, no index exists in `dirObj`
-          key = key + 1 == 4 ? 0 : parseInt(key) + 1;
-
-          newOrientation = this.dirObj[key];
-        }
-      });
-
-      return this.orientation = newOrientation;
-    }
-  }
-
-  setPosition(x, y, f) {
-    this.positionX = x;
-    this.positionY = y;
-    this.orientation = f;
-  }
-
-  checkMove(x, y, gameboard) {
-    if (x < 0) {
-      return false;
-    } else if (y < 0) {
-      return false;
-    } else if (x > gameboard.length) {
-      return false;
-    } else if (y > gameboard.height) {
-      return false;
-    }
-
-      return true;
-  }
+function Robot() {
+  this.positionX;
+  this.positionY;
+  this.orientation;
   
-  // Move by one in the direction of `this.orientation`
-  move(gameboard) {
-    if (this.orientation == 'NORTH') {
-      if ( this.checkMove(this.positionX, this.positionY + 1, gameboard) ) {
-        this.positionY++;
-      }
+  this.directionObj = {
+    0: 'NORTH',
+    1: 'EAST',
+    2: 'SOUTH',
+    3: 'WEST'
+  }
+}
+
+Robot.prototype.move = function(gameboard) {
+  if (this.orientation == 'NORTH') {
+    if ( this.checkMove(this.positionX, this.positionY + 1, gameboard) ) {
+      this.positionY++;
     }
+  }
 
-    if (this.orientation == 'EAST') {
-      if ( this.checkMove(this.positionX + 1, this.positionY, gameboard) ) {
-        this.positionX++;
-      }
-
+  if (this.orientation == 'EAST') {
+    if ( this.checkMove(this.positionX + 1, this.positionY, gameboard) ) {
+      this.positionX++;
     }
-
-    if (this.orientation == 'SOUTH') {
-      if ( this.checkMove(this.positionX, this.positionY - 1, gameboard) ) {
-        this.positionY--;
-      }
-    }
-
-    if (this.orientation == 'WEST') {
-      if ( this.checkMove(this.positionX - 1, this.positionY, gameboard) ) {
-        this.positionX--;
-      }
-    }
-
-    return `${this.positionX},${this.positionY},${this.orientation}`;
 
   }
 
-  // Print current position and orientation
-  report() {
-    return `${this.positionX},${this.positionY},${this.orientation}`;
+  if (this.orientation == 'SOUTH') {
+    if ( this.checkMove(this.positionX, this.positionY - 1, gameboard) ) {
+      this.positionY--;
+    }
   }
+
+  if (this.orientation == 'WEST') {
+    if ( this.checkMove(this.positionX - 1, this.positionY, gameboard) ) {
+      this.positionX--;
+    }
+  }
+
+  return `${this.positionX},${this.positionY},${this.orientation}`;
+}
+
+Robot.prototype.checkMove = function(x, y, bounds) {
+  if (x < 0) {
+    return false;
+  } else if (y < 0) {
+    return false;
+  } else if (x > bounds.length) {
+    return false;
+  } else if (y > bounds.height) {
+    return false;
+  }
+
+    return true;
+}
+
+Robot.prototype.turn = function(direction) {
+  // Rotate robot 90 degrees depending on input
+  if (direction == 'LEFT') {
+    // Move to the orientation left of current orientation
+    // e.g. if current orientation == 'NORTH' and input == 'LEFT', set orientation to 'WEST'
+    // if current orientation == 'EAST' and input == 'RIGHT', set orientation to 'SOUTH'
+    // etc.
+    let newOrientation;
+
+    Object.keys(this.directionObj).forEach(key => {
+      if (this.directionObj[key] == this.orientation) {
+        // validate key:
+        // if key - 1 == -1, no index exists
+        key = key - 1 == -1 ? 3 : parseInt(key) - 1;
+        newOrientation = this.directionObj[key];
+      }
+    });
+
+    return this.orientation = newOrientation;
+
+  } else if (direction == 'RIGHT') {
+    let newOrientation;
+    
+    Object.keys(this.directionObj).forEach(key => {
+      
+      if (this.directionObj[key] == this.orientation) {
+        // validate key:
+        // if key + 1 == 4, no index exists in `directionObj`
+        key = key + 1 == 4 ? 0 : parseInt(key) + 1;
+
+        newOrientation = this.directionObj[key];
+      }
+    });
+
+    return this.orientation = newOrientation;
+  }
+}
+
+Robot.prototype.setPosition = function(x, y, f) {
+  this.positionX = x;
+  this.positionY = y;
+  this.orientation = f;
+}
+
+Robot.prototype.report = function() {
+  return `${this.positionX},${this.positionY},${this.orientation}`;
 }
 
 module.exports = Robot;
