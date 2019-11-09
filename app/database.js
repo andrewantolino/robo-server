9492d2d11e3f8cdec38dc0a29406526ff43e629a
Andrew Antolino <andrew@smithbrothersmedia.com.au>
2019-11-10 10:18:31 +1100

    Create database connection

---
+++ app/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+function connect() {
+  mongoose.connect('mongodb://localhost/robo', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  
+  const db = mongoose.connection;
+
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+    console.log("Database connected");
+  });
+}
+
+module.exports = { connect }
