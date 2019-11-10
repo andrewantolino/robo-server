8033b5bad36250f8b9239c33b40d99c7bab1899e
Andrew Antolino <andrew@smithbrothersmedia.com.au>
2019-11-10 15:59:12 +1100

    Create move and turn instance methods

--- app/models/Robot.js
+++ app/models/Robot.js
@@ -9,6 +9,7 @@ const robotSchema = new mongoose.Schema({
 });
 
 robotSchema.methods.place = function(x, y, f, gameId) {
+
   this.positionX = x;
   this.positionY = y;
   this.orientation = f;
@@ -20,18 +21,58 @@ robotSchema.methods.place = function(x, y, f, gameId) {
     positionY: y,
     orientation: f
   }, {
-    useFindAndModify: false,
     upsert: true,
     new: true
   })
   .exec();
 }
 
-robotSchema.methods.move = function() {
+robotSchema.methods.move = async function(gameBoard) {
 
+  const robot = await this.model('Robot').findOne({ gameId: this.gameId }, (err, res) => {
+    if (err) {
+      console.log('Move error:', err)
+    }
+    if (this.orientation == 'NORTH') {
+      if ( checkMove(this.positionX, this.positionY + 1, gameBoard) ) {
+        this.positionY++;
+      }
+    }
+  
+    if (this.orientation == 'EAST') {
+      if ( checkMove(this.positionX + 1, this.positionY, gameBoard) ) {
+        this.positionX++;
+      }
+  
+    }
+  
+    if (this.orientation == 'SOUTH') {
+      if ( checkMove(this.positionX, this.positionY - 1, gameBoard) ) {
+        this.positionY--;
+      }
+    }
+  
+    if (this.orientation == 'WEST') {
+      if ( checkMove(this.positionX - 1, this.positionY, gameBoard) ) {
+        this.positionX--;
+      }
+    }
+  },{
+    upsert: true,
+    // new: true
+  });
+
+  robot.positionX = this.positionX;
+  robot.positionY = this.positionY;
+
+  robot.save();
+
+  return robot;
 }
 
-robotSchema.statics.checkMove = function(x, y, bounds) {
+robotSchema.statics.checkMove = checkMove;
+
+function checkMove(x, y, bounds) {
   if (x < 0) {
     return false;
   } else if (y < 0) {
@@ -52,7 +93,6 @@ robotSchema.statics.checkPlace = function(orientation) {
     "SOUTH",
     "WEST"
   ];
-  console.log("ori", orientation)
 
   for (dir of orientationArr) {
     if (dir === orientation) {
@@ -61,8 +101,77 @@ robotSchema.statics.checkPlace = function(orientation) {
   }
 }
 
-robotSchema.methods.turn = function(direction) {
+robotSchema.methods.turn = async function(direction) {
+  const directionObj = [
+    'NORTH',
+    'EAST',
+    'SOUTH',
+    'WEST'
+  ];
 
+
+  let newOrientation;
+  // console.log('type',typeof q);
+  // console.log(q instanceof Promise);
+  // console.log(q.exec() instanceof Promise);
+  // const robot = await this.model('Robot').findOne({ gameId: this.gameId }, async (err, res) => {
+  const updatedRobot = this.model('Robot').findOne({ gameId: this.gameId }).exec()
+    .then(async res => {
+
+      const orientationIndex = directionObj.indexOf(res.orientation);
+
+      // Rotate robot 90 degrees depending on input
+      if (direction == 'LEFT') {
+        // Move to the orientation left of current orientation
+        // e.g. if current orientation == 'NORTH' and input == 'LEFT', set orientation to 'WEST'
+        // if current orientation == 'EAST' and input == 'RIGHT', set orientation to 'SOUTH'
+        // etc.
+
+        /**
+         * 
+         * @param {String} direction 
+         * @returns {String}
+         * 
+         */
+        /* function turn(direction) {
+            switch(direction):
+              case "LEFT":
+                Object.keys(directionObj).forEach(key => {
+
+                })
+        } */
+
+        if (orientationIndex != -1) {
+          let key = orientationIndex;
+          key = key - 1 == -1 ? 3 : parseInt(key) - 1;
+
+          newOrientation = directionObj[key];
+          res.orientation = newOrientation;
+
+          const updatedRobot = await res.save();
+
+          return updatedRobot;
+        }
+      } else if (direction == 'RIGHT') {
+
+        if (orientationIndex != -1) {
+          let key = orientationIndex;
+          key = key + 1 == 4 ? 0 : parseInt(key) + 1;
+          
+          newOrientation = directionObj[key];
+          res.orientation = newOrientation;
+          
+          const updatedRobot = await res.save();
+          
+          return updatedRobot;
+        }
+      }
+    })
+    .catch(err => {
+      console.log('Orientation update error:', err)
+    });
+
+  return Promise.resolve(updatedRobot);
 }
 
 robotSchema.methods.report = function() {
